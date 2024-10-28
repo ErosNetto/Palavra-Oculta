a185a9621a7547bfc86fece6fae876e0e274fa68
Eros Netto Antunes <erosnetto1002@gmail.com>
2024-10-28 18:00:09 -0300

    Deploy

--- PalavraOculta/vite.config.js
+++ PalavraOculta/vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Projeto-Palavra-Oculta",
+  base: "/Palavra-Oculta",
 });
